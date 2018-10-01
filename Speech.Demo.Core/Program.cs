1ec84e095051b18709d5a7511705b65eacb32c22
Pavel Karpenko <pakarpen@microsoft.com>
2018-10-01 09:58:33 -0700

    Add project files.

---
+++ Speech.Demo.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Speech.Demo.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
